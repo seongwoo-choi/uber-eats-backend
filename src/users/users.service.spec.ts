55a492da1cd23b3aa583dcb169ac8b9f0b24c869
seongwoo-choi <how0326@naver.com>
2021-11-20 00:08:03 +0900

    Unit Testing UserService
    - Unit Test set up
    - Mocking

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,61 @@
 import { Test } from '@nestjs/testing';
 import { UserService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
+import { JwtService } from '../jwt/jwt.service';
+import { MailService } from '../mail/mail.service';
+
+const mockUserRepository = {
+  find: jest.fn(),
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockVerificationRepository = {
+  delete: jest.fn(),
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  // private async sendEmail => 사용 X
+  sendVerificationEmail: jest.fn(),
+};
 
 describe('UserService', () => {
+  // 모든 테스트에서 UserService 를 사용하고자 한다. => beforeAll 함수의 밖인 최상단에 let service 를 생성
   let service: UserService;
 
+  // 모듈을 만든다 => 이 모듈을 유저 서비스 단 하나만을 갖는다.
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockVerificationRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
     }).compile();
     service = module.get<UserService>(UserService);
   });
