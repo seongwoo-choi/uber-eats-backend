a1df8ae964d8441aafec105c66019e05670b4ff8
csw <hibogo789@gmail.com>
2021-12-06 13:44:25 +0900

    sendVerificationEmail Test

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -4,6 +4,7 @@ import { CONFIG_OPTIONS } from '../common/common.constant';
 import got from 'got';
 import * as FormData from 'form-data';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 jest.mock('got', () => {});
 jest.mock('form-data', () => {
   return {
@@ -35,6 +36,32 @@ describe('MailService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('sendVerifiactionEmail', () => {
+    it('shoul call sendEmail', () => {
+      const sendVerificationArgs = {
+        email: 'email@naver.com',
+        code: 'code',
+      };
+      jest.spyOn(service, 'sendEmail').mockImplementation(async () => {
+        console.log('hi everyone');
+        return true;
+      });
+      service.sendVerificationEmail(
+        sendVerificationArgs.email,
+        sendVerificationArgs.code,
+      );
+
+      expect(service.sendEmail).toHaveBeenCalledTimes(1);
+      expect(service.sendEmail).toHaveBeenCalledWith(
+        'Verify Your Email',
+        'verify-email',
+        [
+          { key: 'code', value: sendVerificationArgs.code },
+          { key: 'username', value: sendVerificationArgs.email },
+        ],
+      );
+    });
+  });
+
   it.todo('sendEmail');
-  it.todo('sendVerificationEmail');
 });
