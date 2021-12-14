90e2c8152791301c9269e75d840926169f89c58f
seongwoo-choi <hibogo789@gmail.com>
2021-12-14 22:36:38 +0900

    Testing createAccount Part One

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -3,6 +3,9 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { getConnection } from 'typeorm';
+import exp from 'constants';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -21,7 +24,35 @@ describe('UserModule (e2e)', () => {
     await app.close();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'csw0326@naver.com';
+    it('should create account', () => {
+      // /graphql 엔드포인트로 posting 을 한다.
+      // graphql 은 엄청 많은 post request 무더기!
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email: "${EMAIL}",
+              password: "1234",
+              role: Owner
+            }) {
+              ok
+              error
+            }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          console.log(res.body);
+          expect(res.body.data.createAccount.ok).toBe('true');
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+  });
+
   it.todo('userProfile');
   it.todo('login');
   it.todo('me');
