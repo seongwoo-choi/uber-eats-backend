06ea0fd3bae8d7e81fd19ed12cbfcd359622f068
seongwoo-choi <hibogo789@gmail.com>
2021-12-15 22:44:29 +0900

    Testing Login

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { getConnection } from 'typeorm';
 import exp from 'constants';
+import { ok } from 'assert';
+import { log } from 'util';
 
 jest.mock('got', () => {
   return {
@@ -13,8 +15,14 @@ jest.mock('got', () => {
 
 const GRAPHQL_ENDPOINT = '/graphql';
 
+const testUser = {
+  email: 'csw0326@naver.com',
+  password: '1234',
+};
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let jwtToken: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -31,7 +39,6 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('createAccount', () => {
-    const EMAIL = 'csw0326@naver.com';
     it('should create account', () => {
       // /graphql 엔드포인트로 posting 을 한다.
       // graphql 은 엄청 많은 post request 무더기!
@@ -41,8 +48,8 @@ describe('UserModule (e2e)', () => {
           query: `
           mutation {
             createAccount(input: {
-              email: "${EMAIL}",
-              password: "1234",
+              email: "${testUser.email}",
+              password: "${testUser.password}",
               role: Owner
             }) {
               ok
@@ -65,8 +72,8 @@ describe('UserModule (e2e)', () => {
           query: `
           mutation {
             createAccount(input: {
-              email: "${EMAIL}",
-              password: "1234",
+              email: "${testUser.email}",
+              password: "${testUser.password}",
               role: Owner
             }) {
               ok
@@ -85,8 +92,74 @@ describe('UserModule (e2e)', () => {
     });
   });
 
+  describe('login', () => {
+    it('should login with correct credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              login(input: {
+                email: "${testUser.email}",
+                password: "${testUser.password}",
+              }) {
+                ok
+                error
+                token
+              }
+            }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          // res.body 를 expect 할 것
+          // res.body 내부의 data 안에서 login 을 가져온다.
+          // res.body.data.login
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          console.log(res);
+          expect(login.ok).toBe('true');
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+
+    it('should not be able to login with wrong credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              login(input: {
+                email: "${testUser.email}",
+                password: "11111",
+              }) {
+                ok
+                error
+                token
+              }
+            }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe('false');
+          expect(login.error).toBe('잘못된 비밀번호입니다.');
+          expect(login.token).toBe(null);
+        });
+    });
+  });
+
   it.todo('userProfile');
-  it.todo('login');
   it.todo('me');
   it.todo('verifyEmail');
   it.todo('editProfile');
